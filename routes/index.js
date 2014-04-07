0a85a579cfb667a0e97999405e4b0150675e2535
lmoore <lawrence2978@hotmail.com>
2014-04-06 20:26:33 -0400

    Added Navigation/routes

--- routes/index.js
+++ routes/index.js
@@ -40,6 +40,9 @@ exports = module.exports = function(app) {
 	app.get('/blog/:category?', routes.views.blog);
 	app.get('/blog/post/:post', routes.views.post);
 	app.get('/gallery', routes.views.gallery);
+    app.get('/amenities', routes.views.amenities);
+    app.get('/floorplans', routes.views.floorplans);
+    app.get('/residents', routes.views.residents);
 	app.all('/contact', routes.views.contact);
 	
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
